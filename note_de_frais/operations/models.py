a53a13e5daab5cd06538f018fdf1bac9aade1110
thomas_cvt <chavotthomas@gmail.com>
2023-09-23 19:33:47 +0200

    Ajout du modèle Opération et ajout des premières instances

--- note_de_frais/operations/models.py
+++ note_de_frais/operations/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Operation(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500, null=True)
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
+    category = models.CharField(max_length=100, null=True)
+    date = models.DateField()
+    refunded = models.DateField(null=True)
