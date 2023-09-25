f43e3eacd45f25b9b490b723e1bc9d5b86781584
Thomas CHAVOT <chavotthomas@gmail.com>
2023-09-25 12:27:29 +0200

    Tri, bouton remboursement

--- note_de_frais/operations/urls.py
+++ note_de_frais/operations/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('mark_as_refunded/', views.mark_as_refunded, name='mark_as_refunded'),
 ]
--- note_de_frais/operations/views.py
+++ note_de_frais/operations/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Operation
-
+from datetime import datetime
 # Create your views here.
 
-def home(request):
-    operations = Operation.objects.all().values()
+def home(request, extras={}):
+    operations = Operation.objects.order_by("-date").values()
     template = loader.get_template("index.html")
     context = {
         "operations": operations
     }
+    for param in extras.keys():
+        context[param] = extras[param]
     return HttpResponse(template.render(context, request))
+
+def mark_as_refunded(request):
+    # https://www.section.io/engineering-education/how-to-build-templates-for-django-applications-with-htmx/
+    # add contact
+    Operation.objects.filter(refunded=None).update(refunded=datetime.now())
+    return home(request, extras={"alert": True, "date": datetime.now().strftime("%d/%m/%Y")})
+
